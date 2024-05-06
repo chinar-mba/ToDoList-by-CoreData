7763ca44ae41edd73c7b5ac65616e50d7ebaf4ea
chinar-mba <chinar.mba@gmail.com>
2024-05-06 11:45:12 +0500

    Learning CoreData

---
+++ ToDOListCoreData/ToDoListItem/ToDoListItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoListItem+CoreDataClass.swift
+//  ToDOListCoreData
+//
+//  Created by Chinar on 15/9/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoListItem)
+public class ToDoListItem: NSManagedObject {
+
+}
